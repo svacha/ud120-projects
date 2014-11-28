dba3b157c810c1b8525691c356fcf33600d31ac9
Geoff Svacha <svacha@gmail.com>
2014-11-28 13:22:58 -0800

    naive bayes exercise

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ud120-projects
 ==============
 
 Starter project code for Udacity ud120.
+
+Run `tools/startup.py` to download and unzip the Enron email dataset.
--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,7 +27,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Traing time: ", round(time() - t0, 3), " s"
+
+t1 = time()
+accuracy = clf.score(features_test, labels_test)
+print "Prediction and accuracy time: ", round(time() - t1, 3), " s"
+
+print "Accuracy on the test data: " + str(accuracy)
 
 #########################################################
 
